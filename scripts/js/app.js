dd904a3df3536ccc6c9d6eb7a274aa3e29d2dd5c
LeOSW42 <leo-serre@legtux.org>
2014-02-11 13:54:35 +0100

    Ajout des pages depuis le menu, ajout des onglets page licence.

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -7,3 +7,33 @@ document.querySelector('#btn-map-back').addEventListener ('click', function () {
   document.querySelector('#map_page').className = 'right skin-dark';
   document.querySelector('[data-position="current"]').className = 'current skin-dark';
 });
+
+// the settings
+document.querySelector('#btn-settings').addEventListener ('click', function () {
+  document.querySelector('#settings_page').className = 'current skin-dark';
+  document.querySelector('[data-position="current"]').className = 'left skin-dark';
+});
+document.querySelector('#btn-settings-back').addEventListener ('click', function () {
+  document.querySelector('#settings_page').className = 'right skin-dark';
+  document.querySelector('[data-position="current"]').className = 'current skin-dark';
+});
+
+// the help
+document.querySelector('#btn-help').addEventListener ('click', function () {
+  document.querySelector('#help_page').className = 'current skin-dark';
+  document.querySelector('[data-position="current"]').className = 'left skin-dark';
+});
+document.querySelector('#btn-help-back').addEventListener ('click', function () {
+  document.querySelector('#help_page').className = 'right skin-dark';
+  document.querySelector('[data-position="current"]').className = 'current skin-dark';
+});
+
+// the license
+document.querySelector('#btn-license').addEventListener ('click', function () {
+  document.querySelector('#license_page').className = 'current skin-dark';
+  document.querySelector('[data-position="current"]').className = 'left skin-dark';
+});
+document.querySelector('#btn-license-back').addEventListener ('click', function () {
+  document.querySelector('#license_page').className = 'right skin-dark';
+  document.querySelector('[data-position="current"]').className = 'current skin-dark';
+});
